43f066a0e9b90cf9c35539c415d80ce77f7e7926
russellsutt <russell.sutter@gmail.com>
2020-12-22 19:14:05 -0800

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,28 @@
 # Enter your procedural solution here!
+require 'pry'
+
+def collect_multiples(limit)
+
+    counter = limit - 1
+    numbers = []
+
+
+    while counter > 0 do
+        if counter % 3 == 0 || counter % 5 == 0
+            numbers.unshift(counter)
+        end
+        counter -= 1
+    end
+
+    return numbers
+end
+
+
+
+def sum_multiples(limit)
+    total = 0
+    collect_multiples(limit).each do |ele|
+        total += ele
+    end
+    return total
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,38 @@
-# Enter your object-oriented solution here!
+
+class Multiples
+
+
+    attr_accessor :limit, :multiples
+
+
+    def initialize(limit)
+        self.limit = limit
+        self.multiples = collect_multiples
+    end
+
+
+
+    def collect_multiples
+
+        counter = self.limit - 1
+        numbers = []
+        while counter > 0 do
+            if counter % 3 == 0 || counter % 5 == 0
+                numbers.unshift(counter)
+            end
+            counter -= 1
+        end
+        return numbers
+    end
+
+
+
+    def sum_multiples
+        total = 0
+        self.multiples.each do |ele|
+            total += ele
+        end
+        return total
+    end
+
+end
